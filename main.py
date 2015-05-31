e71cede4f2fd47b2624e8c75699297836516c723
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2015-06-01 01:33:49 +0300

    Add flask web thingy(under writing) and add multiple users support

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import json
 import subprocess
 from uuid import uuid4 as uuid
 from os import mkdir, chdir, unlink, getcwd
-#from os.path import splitext #for checking files
+from os.path import splitext, exists #for checking files
 
+from uniqify import uniqify
 from urllib.parse import urlparse, parse_qs
 
 import pymongo
 import requests
 
-from flask import Flask, render_template, request, g, session, flash, redirect, url_for
-
-test_mode = True
+#from flask import Flask, render_template, request, g, session, flash, redirect, url_for
 
 #config loader
 config_file = open("config.json", "r")
@@ -54,10 +53,13 @@ def check_and_get_id(link):
 	return qs["list"][0]
 
 
-def get_playlist_info(playlistid, pageToken=None):
-	plid = check_and_get_id(playlistid)
-	if not plid:
-		return False
+def get_playlist_info(link=None, id=None, pageToken=None):
+	if not id:
+		plid = check_and_get_id(link)
+		if not plid:
+			return False
+	else:
+		plid = id
 	if pageToken:
 		req = requests.get(GOOGLE_API_PLAYLISTITEMS, params={'part': 'snippet', 'playlistId': plid, 'key': GOOGLE_API_KEY, 'maxResults': '50', 'pageToken': pageToken})
 	else:
@@ -68,27 +70,37 @@ def get_playlist_info(playlistid, pageToken=None):
 		reqdata = None
 #	if not isinstance(type({}), reqdata): #HELP NEEDED!!!
 #		return False
-	if not reqdata["kind"] == 'youtube#playlistItemListResponse':
-		print("warn: response kind is", reqdata["kind"])
+	if 'error' in reqdata:
+		print("err: ", reqdata["error"]["message"])
+		return False
+	if not ("kind" in reqdata and reqdata['kind'] == 'youtube#playlistItemListResponse'):
+		print("warn: response is", reqdata)
 	return reqdata
 
-def get_video_ids(plid):
+def get_video_ids(link=None, id=None):
+	if id:
+		plinfo = get_playlist_info(id=id)
+	else:
+		plinfo = get_playlist_info(link=link)
 	ids = []
-	plinfo = get_playlist_info(plid)
-	videos_in_playlist = int(plinfo["pageInfo"]["totalResults"])
+	if not plinfo:
+		return False
+	videos_in_playlist = plinfo["pageInfo"]["totalResults"]
 	videos_to_go = videos_in_playlist
-
 	while not videos_to_go == 0:
 		old_ids_len = len(ids)
 		for k in plinfo["items"]:
 			resource_id = k["snippet"]["resourceId"]
 			if not resource_id["kind"] == 'youtube#video':
 				print("err: resource_id['kind'] is", resource_id["kind"])
-				return False
+				continue
 			ids.append(resource_id["videoId"])
 		videos_to_go = videos_in_playlist - len(ids)
 		if not videos_to_go == 0:
-			plinfo = get_playlist_info(plid, plinfo["nextPageToken"])
+			if id:
+				plinfo = get_playlist_info(id=id, pageToken=plinfo["nextPageToken"])
+			else:
+				plinfo = get_playlist_info(link=link, pageToken=plinfo["nextPageToken"])
 	return ids
 
 def exec_ytdl(link, ids, dir):
@@ -109,8 +121,59 @@ def exec_ytdl(link, ids, dir):
 	chdir(CURRENTDIR)
 	unlink(archive)
 
+
 db = open_db()["videos"]
-if test_mode:
+
+def user_thread(username):
+	for k in [d["playlistId"] for d in db.find({"userId": username})]:					#lappa läbi kõik kasutaja <nimi> playlistid
+		print("[Main]: checking {}".format(k))								#kontrolli {}
+		videos = get_video_ids(id=k)									#vaata playlisti {} videoid
+		if not videos:											#kui id ei sobi
+			continue										#võta järgmine
+		stat = db.find_one({"playlistId": k, "userId": username})					#tee jälle sama mis alguses????
+		diff = [d for d in videos]									#kopeeri videote list
+		if len(stat["currentVideos"]) > 0:								#kui andmebaasis on rohkem kui 1 video id
+			for u in zip(stat["currentVideos"], videos):						#lappa läbi ja võta samad välja
+				diff.remove(u[0])
+		base = "/srv/public/nightsnack/{}".format(k)							#playlisti id kaust
+		if not exists(base):										#kausta pole siis tee üks
+			mkdir(base)
+		exec_ytdl("https://www.youtube.com/playlist?list={}".format(k), stat["currentVideos"], base)	# pane youtube-dl tööle
+		db.update({"playlistId": k}, {"$set": {"currentVideos": videos}})				#ning uuenda playlisti infot
+	print("[Main]: ending updates for user {}".format(username))
+
+def main():
+	print("[Main]: Starting up")
+	for k in uniqify([d["userId"] for d in db.find()]):
+		print("[Main]: Looking playlist updates for user {}".format(k))
+		user_thread(k)
+
+def adduser():
+	username = "mikroskeem"
+	playlists = [
+		"https://www.youtube.com/playlist?list=PLGE39Wpa-qf1xjp4gmJ_1PBzH7a_-GdOe",
+		"https://www.youtube.com/playlist?list=PLGE39Wpa-qf3PNgSiXuT9qkv2EK1-3WE7",
+		"https://www.youtube.com/playlist?list=PLGE39Wpa-qf0bohzuPl5MnT2v2QyDD7pr",
+		"https://www.youtube.com/playlist?list=PLGE39Wpa-qf2x7agzPsAGdEfKxIAWA7Jv",
+		"https://www.youtube.com/playlist?list=PLGE39Wpa-qf2x7agzPsAGdEfKxIAWA7Jv"
+	]
+	for k in playlists:
+		plid = check_and_get_id(k)
+		db.insert({"userId": username, "currentVideos": [], "playlistId": plid})
+
+main()
+
+close_db()
+
+"""
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+	if 'userid' in session:
+		db = open_db()
+		if db.find_one({"id": session["userid"]}) == None:
+		if test_mode:
 	stat = db.find_one({"playlistId": "PLGE39Wpa-qf3PNgSiXuT9qkv2EK1-3WE7"})
 	if not stat:
 		db.insert({"userId": "markv", "currentVideos": get_video_ids(TEST_URL), "playlistId": "PLGE39Wpa-qf3PNgSiXuT9qkv2EK1-3WE7"})
@@ -123,17 +186,7 @@ if test_mode:
 		diff.remove(k[0])
 	exec_ytdl(TEST_URL, stat["currentVideos"], "/home/mark/nightsnack-test")
 
-close_db()
-
-"""
-app = Flask(__name__)
-
-@app.before_request
-def before_request():
-	if 'userid' in session:
-		db = open_db()
-		if db.find_one({"id": session["userid"]}) == None:
-			session.pop('userid')
+	session.pop('userid')
 
 def flask_open_db():
 	if not hasattr(g, 'db'):
